3719d0ad22aea6849311f5ae02d34db3c8ba782e
MariSena331 <mariana7007@hotmail.com>
2021-04-30 21:30:08 -0300

    Consturindo primeiros componentes, inserindo polished e react icons;
    Signed-off-by: MariSena331 <mariana7007@hotmail.com>

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Container, Title } from './styles'
+import logoImg from '../../assets/logo.svg'
+
+import { Container, Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
     return (
         <Container>
+            <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no GitHub</Title>
+
+            <Form action="">
+                <input placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img
+                       src="https://avatars.githubusercontent.com/u/35273542?s=400&u=b752b187009348e33521a6bfa0c606339541c042&v=4"
+                        alt="Mariana Sena"
+                    />
+                    <div>
+                        <strong>gravateca/index</strong>
+                        <p>Cadastre seus livros aqui!</p>
+                    </div>
+
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories>
         </Container>
     )
 }
